3594d2fa59f499b6ac7c12dbcac6b0a9d5198d32
Your Name <you@example.com>
2024-08-18 12:16:39 -0700

    first commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - program starts and ends in this function
+ * takes no arguments
+ *
+ * Description: determine the size of various data types on a computer
+ * Return: returns 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+	return (0);
+}
+
