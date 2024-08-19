2fb3f3779dc294913bacb81fcfc7c0f0c04b12bb
Your Name <you@example.com>
2024-08-19 12:22:47 -0700

    hot ex

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337.
+ * @str: Pointer to string.
+ *
+ * Return: Returns the pointer to the modified string.
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char *alpha = "aAeEoOtTlL";
+	char *num = "4433007711";
+
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == alpha[j])
+			{
+				str[i] = num[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
+
